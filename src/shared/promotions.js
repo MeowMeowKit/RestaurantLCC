56dc2bfc1e56636ed079d46ab369bd0c905acfaa
MeowMeowKit <92076631+MeowMeowKit@users.noreply.github.com>
2022-08-07 18:25:46 +0700

    Assignment

---
+++ src/shared/promotions.js
@@ -0,0 +1,12 @@
+export const PROMOTIONS = [
+   {
+      id: 0,
+      name: "Weekend Grand Buffet",
+      image: "/assets/images/buffet.png",
+      label: "New",
+      price: "19.99",
+      featured: true,
+      description:
+         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eleifend nibh sit amet metus ultrices, id hendrerit erat convallis. Suspendisse mi libero, porttitor vehicula ex eu, fringilla blandit massa. Quisque et efficitur risus. Integer augue est, venenatis sit amet eros ullamcorper, consequat laoreet urna.",
+   },
+];
